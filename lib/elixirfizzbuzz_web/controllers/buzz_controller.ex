ed404e5d2ca0bfc68f2512bc8c3aff7efd19af3d
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-14 16:41:33 -0300

    buzz controller config

--- lib/elixirfizzbuzz_web/controllers/buzz_controller.ex
+++ lib/elixirfizzbuzz_web/controllers/buzz_controller.ex
@@ -2,7 +2,16 @@ defmodule ElixirfizzbuzzWeb.BuzzController do
   use ElixirfizzbuzzWeb, :controller
 
   def index(conn, _params) do
+    buzz = fn
+      0, _ -> "Buzz"
+      _, n -> n
+    end
+
+    buzzCatcher = fn n ->
+      buzz.(rem(n, 5), n)
+    end
+
     conn
-    |> json(%{})
+    |> json(%{buzz: Enum.map(1..100, buzzCatcher)})
   end
 end
