9025a3525fab71c200460fd756859a0127585e41
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-14 16:55:01 -0300

    fizzbuzz controller config

--- lib/elixirfizzbuzz_web/controllers/fizzbuzz_controller.ex
+++ lib/elixirfizzbuzz_web/controllers/fizzbuzz_controller.ex
@@ -2,7 +2,18 @@ defmodule ElixirfizzbuzzWeb.FizzBuzzController do
   use ElixirfizzbuzzWeb, :controller
 
   def index(conn, _params) do
+    fizzbuzz = fn
+      0, 0, _ -> "FizzBuzz"
+      0, _, _ -> "Fizz"
+      _, 0, _ -> "Buzz"
+      _, _, n -> n
+    end
+
+    fizzbuzzCatcher = fn n ->
+      fizzbuzz.(rem(n, 3), rem(n, 5), n)
+    end
+
     conn
-    |> json(%{})
+    |> json(%{fizzbuzz: Enum.map(1..100, fizzbuzzCatcher)})
   end
 end
