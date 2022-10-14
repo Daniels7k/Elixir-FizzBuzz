e1eda1158fc44a934f87d919ec95cc252583131c
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-10-14 18:10:20 -0300

    changing function name

--- lib/elixirfizzbuzz_web/controllers/other_controller.ex
+++ lib/elixirfizzbuzz_web/controllers/other_controller.ex
@@ -2,11 +2,11 @@ defmodule ElixirfizzbuzzWeb.OtherController do
   use ElixirfizzbuzzWeb, :controller
 
   def index(conn, _params) do
-    fizzbuzzCatcher = fn n ->
+    otherCatcher = fn n ->
       rem(n, 3) != 0 && rem(n, 5) != 0
     end
 
     conn
-    |> json(%{other: Enum.filter(1..100, fizzbuzzCatcher)})
+    |> json(%{other: Enum.filter(1..100, otherCatcher)})
   end
 end
